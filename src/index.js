109c036526c264c7dc78ed051f6770a436491580
Alexis Parave <aparavev@gmail.com>
2023-08-02 17:34:11 -0500

    :sparkles: Versioning api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-const express = require("express"); 
+const express = require("express");
+const v1Router = require("./v1/routes") 
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
-// For testing purposes 
-app.get("/", (req, res) => { 
-    res.send("<h2>It's Working!</h2>"); 
-}); 
+app.use('/api/v1', v1Router)
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
