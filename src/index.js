04cfa238266cd8261d6d167c9e843398f2d404e1
Alexis Parave <aparavev@gmail.com>
2023-08-02 17:39:45 -0500

    :sparkles: Add workout routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const v1Router = require("./v1/routes") 
+const v1WorkoutRouter = require("./v1/routes/workout.routes")
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
 app.use('/api/v1', v1Router)
+app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
---
+++ src/v1/routes/workout.routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Get all workouts");
+});
+
+router.get("/:workoutId", (req, res) => {
+  res.send("Get an existing workout");
+});
+
+router.post("/", (req, res) => {
+  res.send("Create a new workout");
+});
+
+router.patch("/:workoutId", (req, res) => {
+  res.send("Update an existing workout");
+});
+
+router.delete("/:workoutId", (req, res) => {
+  res.send("Delete an existing workout");
+});
+
+module.exports = router;
