43a6e346c96e39d1f0709abd1b61a2df2689fdc2
Alexis Parave <aparavev@gmail.com>
2023-08-02 19:28:25 -0500

    :sparkles: Add workoutService

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService = require('../services/workoutService')
+
 const getAllWorkouts = (req, res) => {
+  const workouts = workoutService.getAllWorkouts()
   res.send("Get all workouts");
 };
 
 const getOneWorkout = (req, res) => {
+  const workout = workoutService.getOneWorkout()
   res.send("Get an existing workout");
 };
 
 const createNewWorkout = (req, res) => {
+  const newWorkout = workoutService.createNewWorkout()
   res.send("Create a new workout");
 };
 
 const updateOneWorkout = (req, res) => {
+  const workout = workoutService.updateOneWorkout()
   res.send("Update an existing workout");
 };
 
 const deleteOneWorkout = (req, res) => {
+  workoutService.deleteOneWorkout()
   res.send("Delete an existing workout");
 };
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,27 @@
+const getAllWorkouts = () => {
+  return;
+};
+
+const getOneWorkout = () => {
+  return;
+};
+
+const createNewWorkout = () => {
+  return;
+};
+
+const updateOneWorkout = () => {
+  return;
+};
+
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
