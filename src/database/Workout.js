1eae6b48c63e3bbbb1ca405c84b94e129eb3e937
Alexis Parave <aparavev@gmail.com>
2023-08-02 20:06:39 -0500

    :sparkles: Add functionality to create a new workout

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,7 +1,21 @@
 const DB = require("./db.json");
+const { saveToDatabase } = require("./utils");
 
 const getAllWorkouts = () => {
   return DB.workouts;
 };
 
-module.exports = { getAllWorkouts };
+const createNewWorkout = (newWorkout) => {
+  const isAlreadyAdded = DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+  
+  if (isAlreadyAdded) return;
+
+  DB.workouts.push(newWorkout);
+  saveToDatabase(DB);
+  return newWorkout;
+};
+
+module.exports = {
+  getAllWorkouts, 
+  createNewWorkout,
+};
