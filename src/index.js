fb7c6ab67cbea161f42b790ae943fff747930192
Alexis Parave <aparavev@gmail.com>
2023-08-03 19:56:07 -0500

    :memo: Setup swagger to add api documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const v1Router = require("./v1/routes") 
 const v1WorkoutRouter = require("./v1/routes/workout.routes")
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use('/api/v1', v1Router)
 app.use('/api/v1/workouts', v1WorkoutRouter)
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
+    V1SwaggerDocs(app, PORT);
 });
